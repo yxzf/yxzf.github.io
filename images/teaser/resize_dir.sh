ff3b0fa0b9292953265da4e9e8b2fd8cfcf724e0
yxzf <shenchengguang@gmail.com>
2017-03-17 20:38:18 +0800

    resize img

---
+++ images/teaser/resize_dir.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+#echo "convert file:$1"
+#convert "$1"  -background none -gravity center -resize 400x250 -extent 400x250 "$1"
+files=$(ls $1)
+echo $files
+for file in $files; do
+    convert "$1/$file"  -background none -gravity center -resize 400x250 -extent 400x250 "$1/$file"
+done
