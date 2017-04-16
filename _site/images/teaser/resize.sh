cef9c2e1902a3e7967d08c620a66e1b88a2618c7
yxzf <shenchengguang@gmail.com>
2017-04-16 20:11:15 +0800

    latex

--- _site/images/teaser/resize.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-echo "convert file:$1"
-convert "$1"  -background none -gravity center -resize $2 -extent $2 "$1"
--- _site/images/teaser/resize_dir.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-#echo "convert file:$1"
-#convert "$1"  -background none -gravity center -resize 400x250 -extent 400x250 "$1"
-files=$(ls $1)
-echo $files
-for file in $files; do
-    convert "$1/$file"  -background none -gravity center -resize $2 -extent $2 "$1/$file"
-done
