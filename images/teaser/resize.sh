72fd2a17e6a0d080d3cad1bd381cbd4117d994f1
yxzf <shenchengguang@gmail.com>
2017-04-16 16:31:33 +0800

    resize

--- images/teaser/resize.sh
+++ images/teaser/resize.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 echo "convert file:$1"
-convert "$1"  -background none -gravity center -resize 400x250 -extent 400x250 "$1"
+convert "$1"  -background none -gravity center -resize $2 -extent $2 "$1"
